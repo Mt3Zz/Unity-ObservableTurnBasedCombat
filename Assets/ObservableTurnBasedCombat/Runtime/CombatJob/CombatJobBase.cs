6e5fb63de2750d846abcd674c812a930f991af44
administer <ttttt2tttttt+gituser01@gmail.com>
2024-05-17 18:29:17 +0900

    Add CombatJob class and Test

---
+++ Assets/ObservableTurnBasedCombat/Runtime/CombatJob/CombatJobBase.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace ObservableTurnBasedCombat
+{
+    internal abstract class CombatJobBase
+    {
+
+    }
+}
---
+++ Assets/ObservableTurnBasedCombat/Tests/PlayMode/CombatJob/CombatJobTest.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+
+/*
+ * Test List
+ * 
+ */
+
+
+namespace ObservableTurnBasedCombat.Tests.PlayMode
+{
+    public class CombatJobTest
+    {
+        // A Test behaves as an ordinary method
+        [Test]
+        public void CombatJobTestSimplePasses()
+        {
+            // Use the Assert class to test conditions
+        }
+
+        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+        // `yield return null;` to skip a frame.
+        [UnityTest]
+        public IEnumerator CombatJobTestWithEnumeratorPasses()
+        {
+            // Use the Assert class to test conditions.
+            // Use yield to skip a frame.
+            yield return null;
+        }
+    }
+}
